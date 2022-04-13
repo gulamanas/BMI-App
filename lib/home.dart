3a7d5cbb4b26a3be09dcb88ac82c61d04c058355
Gulam Anas <g.anas786@gmail.com>
2022-04-13 23:57:15 +0530

    added input field

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatefulWidget {
   const Home({ Key? key }) : super(key: key);
@@ -13,7 +14,43 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI Application'),
+        centerTitle: true,
       ),
+      body: Column(
+        children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/bmilogo.png'),
+          ],
+        ),
+        Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: '32',
+                label: Text('Age'),
+                icon: Icon(Icons.person)
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: '5.4',
+                label: Text('Height in Feet'),
+                icon: Icon(Icons.table_chart)
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: '180',
+                label: Text('Weight In lb'),
+                icon: Icon(Icons.table_rows_outlined)
+              ),
+            )
+          ],
+        )
+      ],
+      )
     );
   }
 }
