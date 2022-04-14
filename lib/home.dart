4c35b8db944b89bef4d494a57d723b9c4fa577d6
Gulam Anas <g.anas786@gmail.com>
2022-04-14 10:56:00 +0530

    extracting widget

--- lib/home.dart
+++ lib/home.dart
@@ -24,41 +24,79 @@ class _HomeState extends State<Home> {
             Image.asset('assets/bmilogo.png'),
           ],
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 16.0),
-          child: Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: '32',
-                  label: Text('Age'),
-                  prefixIcon: Icon(Icons.person)
-                ),
-              ),
-              SizedBox(height: 8.0,),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: '5.4',
-                  label: Text('Height in Feet'),
-                  prefixIcon: Icon(Icons.table_chart)
-                ),
-              ),
-              SizedBox(height: 8.0,),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: '180',
-                  label: Text('Weight In lb'),
-                  prefixIcon: Icon(Icons.table_rows_outlined)
-                ),
-              )
-            ],
+        Container(
+          color: Colors.grey[300],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+            child: Column(
+              children: [
+                Age(),
+                SizedBox(height: 8.0,),
+                Height(),
+                SizedBox(height: 8.0,),
+                Weight(),
+
+                ElevatedButton(onPressed: () {}, child: Text('Calculate')),
+              ],
+            ),
           ),
         )
       ],
       )
+    );
+  }
+}
+
+class Age extends StatelessWidget {
+  const Age({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: '32',
+        label: Text('Age'),
+        prefixIcon: Icon(Icons.person)
+      ),
+    );
+  }
+}
+
+class Height extends StatelessWidget {
+  const Height({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: '5.4',
+        label: Text('Height in Feet'),
+        prefixIcon: Icon(Icons.table_chart)
+      ),
+    );
+  }
+}
+
+class Weight extends StatelessWidget {
+  const Weight({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: '180',
+        label: Text('Weight In lb'),
+        prefixIcon: Icon(Icons.table_rows_outlined)
+      ),
     );
   }
 }
